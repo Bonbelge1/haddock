71e9a506d8087d4dd3dd59ea9f2a2701e7c96686
Frederick Beaulieu <frederick.beaulieu@hotmail.com>
2017-02-11 15:28:15 -0500

    added resistance.py who return the water resistance at a certain speed

---
+++ simulation/resistance.py
@@ -0,0 +1,13 @@
+from xlrd import open_workbook
+
+class resistance(inducedSpeed)
+   return intresistance
+
+wb = open_workbook('resistance.xls')
+
+int col = 1
+
+while sheet.cell((1, col).value) < inducedSpeed
+	col = col + 1
+
+resistance = inducedSpeed / sheet.cell((1, col).value) * sheet.cell((2, col).value)
